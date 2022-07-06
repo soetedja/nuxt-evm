1a89b04886aed1dcbd64a1ebcad54b15af45bcca
soetedja <soetedja@gmail.com>
2022-07-06 15:34:07 +0700

    Initial Setup

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nuxt-link to="/">Home</nuxt-link>
+    <h1>AboutPage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nuxt-link to="/about">About</nuxt-link>
+    <Tutorial/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
